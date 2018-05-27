94b88f27d92eb0656ed3f026abb5e835eaa4c93e
Murtada al Mousawy <murtada.al.mousawy@gmail.com>
2018-05-27 19:50:31 +0200

    Add alternative animation for IE because mix-blend-mode is not available. Fix blurriness of assets

--- intro.js
+++ intro.js
@@ -10,9 +10,11 @@
 	var torchLights = document.querySelectorAll('.header__torch-light');
 	var animationDone = false;
 	var introEnd = false;
-	var spotlightScale = 8;
+	var spotlightScaleStart = 8;
 	var spotlightScaleEnd = 30;
+	var spotlightScale = spotlightScaleStart;
 	var centerScreenX = window.innerWidth * .5;
+	var blendingAvailable = (window.getComputedStyle(document.body).mixBlendMode !== undefined);
 
 	pip.addEventListener('animationend', function() {
 		spotlight.classList.add('is-revealed');
@@ -25,12 +27,12 @@
 
 		setTimeout(function() {
 			torches[0].classList.add('is-lit');
-			torchLights[0].classList.add('is-lit');
+			torchLights[0].classList.add((blendingAvailable ? 'is-lit' : 'is-lit-ie'));
 		}, 350);
 
 		setTimeout(function() {
 			torches[1].classList.add('is-lit');
-			torchLights[1].classList.add('is-lit');
+			torchLights[1].classList.add((blendingAvailable ? 'is-lit' : 'is-lit-ie'));
 		}, 550);
 
 		animationDone = true;
@@ -44,6 +46,7 @@
 			var pipPosition = pip.getBoundingClientRect();
 			locationX = pipPosition.left + pipPosition.width * .5 + 'px';
 			locationY = pipPosition.top + pipPosition.height * .5 + 'px';
+			spotlightScale = spotlightScaleStart + (Math.random() - Math.random()) * .3;
 		} else {
 			locationX = centerScreenX + 'px';
 			locationY = '60%';
@@ -61,7 +64,6 @@
 			'transparent ' + (spotlightScale * .3) + 'rem,' +
 			'rgba(0, 0, 0, 0.15) ' + (spotlightScale * .6) + 'rem,' +
 			'rgba(0, 0, 0, 0.85) ' + spotlightScale + 'rem)';
-
 
 		if (!introEnd) {
 			window.requestAnimationFrame(animationStep);
